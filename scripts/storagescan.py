d8117c36e6ab2a763a02e425a3aebbea872601a7
takochan <gianxiann@gmail.com>
2025-04-25 13:39:40 +0700

    Update storagescan.py

--- scripts/storagescan.py
+++ scripts/storagescan.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 import time
 from web3 import Web3
-from web3automation import crypt256
+from web3automation import crypt265
 from eth_account import Account
 from colorama import init, Fore, Style
 
